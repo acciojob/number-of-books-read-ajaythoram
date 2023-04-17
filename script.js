f6e8d3a0c7a522667c705f08ce7e0506de867528
ajaythoram <115677014+ajaythoram@users.noreply.github.com>
2023-04-17 14:57:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -19,8 +19,10 @@ const library = [
 const numberOfBooksRead = () => {
   // write your code here
 	let count=0;
-	for(var ele of library){
-		if(ele.readingStatus === true) count++;
+	for(int i = 0;i<library.length;i++){
+        if(library.readingStatus === true){
+        count++;
+}
 	}
 	console.log(count);
 	 }
